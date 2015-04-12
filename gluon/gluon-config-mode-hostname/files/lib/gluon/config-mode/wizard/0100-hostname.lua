2459401a62da328165f2f791df876ffbe4e6a09c
@RubenKelevra <ruben@freifunk-nrw.de>
2015-04-12 19:10:03 +0200

    Update 0100-hostname.lua

--- gluon/gluon-config-mode-hostname/files/lib/gluon/config-mode/wizard/0100-hostname.lua
+++ gluon/gluon-config-mode-hostname/files/lib/gluon/config-mode/wizard/0100-hostname.lua
@@ -8,16 +8,14 @@ function M.section(form)
   local s = form:section(cbi.SimpleSection, nil, nil)
   local o = s:option(cbi.Value, "_hostname", i18n.translate("Node name"))
   local tmp = uci:get_first("system", "system", "hostname")
-  #replace all underlines with spaces
   tmp = string.gsub(tmp, "_", " ")
   o.value = tmp
   o.rmempty = false
-  o.datatype = "type(hostname, ' ')"
+  o.datatype = "hostname_w_spaces"
 end
 
 function M.handle(data)
   local tmp = data._hostname
-  #replace all spaces with underlines
   tmp = string.gsub(tmp, " ", "_")
   uci:set("system", uci:get_first("system", "system"), "hostname", tmp)
   uci:save("system")
