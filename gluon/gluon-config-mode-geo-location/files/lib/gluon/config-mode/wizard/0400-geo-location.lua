794548d9b96124a9fdc237ef282a9b968197da5f
@RubenKelevra <ruben@freifunk-nrw.de>
2015-04-28 02:04:20 +0200

    Update 0400-geo-location.lua

--- gluon/gluon-config-mode-geo-location/files/lib/gluon/config-mode/wizard/0400-geo-location.lua
+++ gluon/gluon-config-mode-geo-location/files/lib/gluon/config-mode/wizard/0400-geo-location.lua
@@ -6,13 +6,13 @@ local M = {}
 function M.section(form)
   local s = form:section(cbi.SimpleSection, nil,
     [[Um deinen Knoten auf der Karte anzeigen zu können, benötigen
-    wir seine Koordinaten. Hier hast du die Möglichkeit, diese zu
-    hinterlegen.]])
+    wir seine Koordinaten. Hier hast du die Möglichkeit, diese
+    manuell festzulegen.]])
 
   local o
 
-  o = s:option(cbi.Flag, "_location", "Knoten auf der Karte anzeigen")
-  o.default = uci:get_first("gluon-node-info", "location", "share_location", o.disabled)
+  o = s:option(cbi.Flag, "_location", "Eigene Koordinaten eingeben")
+  o.default = uci:get_first("gluon-node-info", "location", "user_defined_location", o.disabled)
   o.rmempty = false
 
   o = s:option(cbi.Value, "_latitude", "Breitengrad")
@@ -20,20 +20,20 @@ function M.section(form)
   o:depends("_location", "1")
   o.rmempty = false
   o.datatype = "float"
-  o.description = "z.B. 53.873621"
+  o.description = "z.B. 51.873621"
 
   o = s:option(cbi.Value, "_longitude", "Längengrad")
   o.default = uci:get_first("gluon-node-info", "location", "longitude")
   o:depends("_location", "1")
   o.rmempty = false
   o.datatype = "float"
-  o.description = "z.B. 10.689901"
+  o.description = "z.B. 7.689901"
 end
 
 function M.handle(data)
   local sname = uci:get_first("gluon-node-info", "location")
 
-  uci:set("gluon-node-info", sname, "share_location", data._location)
+  uci:set("gluon-node-info", sname, "user_defined_location", data._location)
   if data._location and data._latitude ~= nil and data._longitude ~= nil then
     uci:set("gluon-node-info", sname, "latitude", data._latitude)
     uci:set("gluon-node-info", sname, "longitude", data._longitude)
